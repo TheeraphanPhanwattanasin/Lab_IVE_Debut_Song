6605bb7c4ccefbf13483e7365eb668e52c566841
boss <theeraphan_p@cmu.ac.th>
2023-01-03 15:56:37 +0700

    sed leo

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,28 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    double sum = 0, sumpow = 0, mean;
+    string text;
+    
+    ifstream score("score.txt");
+    
+    while(getline(score,text)){
+        sum += atof(text.c_str());
+        sumpow += pow(atof(text.c_str()),2);
+        count++;
+    }
+
+    mean = sum/count;
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << sqrt((sumpow/count)- pow(mean,2));
+}
